4ac198edf08aee660f2a02b24444b2c85240077f
nehasaraswat106 <nehasaraswat106@gmail.com>
2024-11-23 11:36:40 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,14 +1,17 @@
 variable "instance_type" {
   description = "Type of EC2 instance"
   type        = string
+  default     = "t2.micro"
 }
 
 variable "ami" {
   description = "AMI ID for the EC2 instance"
   type        = string
+  default     = "ami-0866a3c8686eaeeba"
 }
 
 variable "key_name" {
   description = "Key pair name"
   type        = string
+  default     = "terra-key"
 }
