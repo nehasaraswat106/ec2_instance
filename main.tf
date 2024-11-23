11ab583abf045da30964f0473b1c9464fb8a0223
nehasaraswat106 <nehasaraswat106@gmail.com>
2024-11-23 14:07:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,13 @@
-resource "aws_instance" "example" {
+resource "aws_instance" "ec2_instance" {
   ami           = var.ami
   instance_type = var.instance_type
   key_name      = var.key_name
+
   tags = {
-    Name = "EC2Instance"
+    Name = "EC2 Instance"
   }
+}
+
+output "instance_id" {
+  value = aws_instance.ec2_instance.id
 }
